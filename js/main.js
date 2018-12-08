0b39b49ef7a3352f6b7cb44f626a98a299b181d4
Henry T <henryt.inc@gmail.com>
2018-12-08 00:32:56 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#secret').click(function() {
+        //var name = prompt('What is your name?');
+
+        //alert('Why hello there, ' + name);
+
+        swal({
+            text: 'What is your name?',
+            content: "input",
+            button: {
+                text: "Submit",
+                closeModal: false,
+            },
+        })
+        .then(name => {
+            if (!name) throw null;
+
+            if (name.toLowerCase() == 'karl') {
+                swal('Ono!', 'no u', 'warning');
+            } else {
+                swal('Wow!', 'Hello there, ' + name + '!', 'success');
+            }
+
+        })
+        .catch(err => {
+            if (err) {
+                swal("Oh noes!", "You broke something", "error");
+            } else {
+                swal.close();
+            }
+        });
+    });
+
+    $('#attacc').click(function() {
+        swal('Attacking!!!');
+        $('#blog').css('background', 'url("https://amp.thisisinsider.com/images/5ad746638a376dbe528b45a3-750-563.jpg")');
+        $('h4').html('penguins are great!');
+    });
+});
